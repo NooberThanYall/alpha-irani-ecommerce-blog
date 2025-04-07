8106fa7c8d03ad8095f46d60d103c679760b25ba
NooberThanYall <nooberthanyall@gmail.com>
2025-04-07 20:43:16 +0300

    env added for mongo

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+   const body = await req.json();
+
+   try {
+      
+   } catch (error) {
+      
+   }
+}
